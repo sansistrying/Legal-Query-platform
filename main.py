ef7ec94f57f3ec324d99c5fd7f3dfc945127c5d1
SANSITA KARTHIKEYAN <114069040+sansistrying@users.noreply.github.com>
2024-06-25 20:23:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,14 +13,37 @@ os.environ["LANGCHAIN_TRACING_V2"] = "true"
 os.environ["LANGCHAIN_ENDPOINT"] = "https://api.smith.langchain.com"
 os.environ["LANGCHAIN_API_KEY"] = "your-api-key"
 os.environ["TAVILY_API_KEY"] = "your-api-key"
+import os
+from crewai import Task
+from agents import Law_Agents
+from tasks import Law_Tasks
+from crewai import Crew, Process
+import streamlit as st
+from dotenv import load_dotenv
+load_dotenv()
+
+
+# Groq API base_url 
+os.environ["OPENAI_API_BASE"] = 'https://api.groq.com/openai/v1' 
+
+# Model you wish to use, see https://console.groq.com/docs/models 
+os.environ["OPENAI_MODEL_NAME"] = 'llama3-70b-8192' 
+
+# Your Groq API key
+os.environ["OPENAI_API_KEY"] = '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'  
+os.environ["GROQ_API_KEY"] = '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'  
+
+os.environ["TAVILY_API_KEY"] = "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"
+os.environ["AI21_API_KEY"] = "x1WB5Ld48GPBn8jlzG3T1dsuJvd8R0Ny"
+
 
 class Law_Crew:
     def __init__(self, query):
-        self.query = query  # Initialize the class with the query
+        self.query = query
 
     def run(self):
-        agents = Law_Agents()  # Instantiate the Law_Agents class
-        tasks = Law_Tasks()  # Instantiate the Law_Tasks class
+        agents = Law_Agents()
+        tasks = Law_Tasks()
 
         # Create individual search agents
         pdf_searcher = agents.pdf_searcher()
@@ -35,32 +58,28 @@ class Law_Crew:
 
         # Define the master task that summarizes the content
         master_task = Task(
-            description='summarize the content given to you and add the details such as article number or citation of a law etc in the answer from the content',
-            expected_output='An answer to the question in 2 to 3 lines and be in detail.',
+            description='summarize the content given to you and add the details such as article number or citation of a law and links etc in the answer from the content',
+            expected_output='An answer to the question in 2 to 3 lines and be in detail.An answer along with list of urls and pdf from where the answer was taken from',
             agent=master_agent,
             context=[website_search_task, web_search_task, pdf_search_task]
         )
 
-        # Create a crew with the defined agents and tasks
         crew = Crew(
             agents=[master_agent],
-            tasks=[website_search_task, pdf_search_task, web_search_task, master_task],
-            verbose=1,  # Enable verbose output for debugging
-            full_output=True,  # Enable full output logging
-            process=Process.sequential,  # Run tasks sequentially
-            output_log_file=True,  # Enable output logging to a file
+            tasks=[pdf_search_task, website_search_task, web_search_task, master_task],
+            verbose=1,
+            full_output=True,
+            process=Process.sequential,
+            output_log_file=True,
         )
 
-        # Run the crew with the input query and return the result
         result = crew.kickoff(inputs={'query': self.query})
-
         return master_task.output.exported_output
 
 if __name__ == "__main__":
-    # Entry point for the script
     print("Welcome to Law Query Platform")
     print('-----------------------------')
-    query = input("enter your query:")  # Prompt user for a query
-    law_crew = Law_Crew(query)  # Instantiate the Law_Crew class with the query
-    result = law_crew.run()  # Run the Law_Crew and get the result
-    print(result)  # Print the result
+    query=input("Enter your query:")
+    law_crew=Law_Crew(query)
+    result=law_crew.run()
+    print(result)
