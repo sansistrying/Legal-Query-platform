23dd186710c3edf6e4da9795321f0670fb5aa3e2
SANSITA KARTHIKEYAN <114069040+sansistrying@users.noreply.github.com>
2024-06-25 20:24:47 +0530

    Update agents.py

--- agents.py
+++ agents.py
@@ -10,28 +10,32 @@ class Law_Agents():
             role='Legal Document Analyst',
             goal='To meticulously scrutinize legal documents in PDF format, extracting information to respond to queries and then return the answer to the question as well as the source of the answer.',
             backstory='PDF_searcher is a seasoned legal professional with a specialization in document analysis. With a background in law and years of experience in handling intricate legal documents, PDF_searcher possesses a deep understanding of legal terminology and nuances. Their expertise allows them to navigate through complex texts efficiently, extracting key details and providing answers to complex legal inquiries.',
-            tools=[PDF_search()]
+            tools=[PDF_search()],
+            allow_delegation = True
             )
     
     def website_searcher(self):
         return Agent(
             role='Legal Website Analyst',
-            goal='To scour legal websites and online databases for relevant information, ensuring accurate and reliable responses to queries.',
+            goal='To search the text, and answer the question if possible. If not found reply that answer is not available and give up. Do not hallucinate.',
             backstory='website_searcher is a proficient legal researcher adept at navigating the vast landscape of online legal resources. With a background in law and extensive experience in online research, website_searcher possesses a keen eye for identifying credible sources and extracting pertinent information. Their familiarity with various legal platforms enables them to swiftly locate relevant data and deliver well-researched answers to complex legal questions.',
-            tools=[website_search()]
+            tools=[website_search()],
+            allow_delegation = True
             )
     
     def web_searcher(self):
+        tavily=TavilySearchResults(k=3,max_results=5)
         return Agent(
             role='Legal Web Searcher',
-            goal='To conduct web searches to retrieve legal information, providing responses to inquiries. try to provide as accurate as possible.',
-            backstory='web_searcher is an adept legal researcher skilled in harnessing the power of internet search engines to gather comprehensive legal information. With a background in law and a passion for staying abreast of legal developments, web_searcher possesses a knack for conducting targeted searches and filtering through vast amounts of data to find relevant insights. Their proficiency in online research methodologies makes them a valuable asset in delivering timely and informed responses to legal queries.',
-            tools=[TavilySearchResults(k=3)]
+            goal='To conduct web searches to retrieve legal information, providing responses to inquiries if possible. Do not hallucinate. Give the answer in just one try.',
+            backstory='web_searcher is an adept legal researcher skilled in harnessing the power of internet search engines to gather comprehensive legal information. With a background in law and a passion for staying abreast of legal developments, web_searcher possesses a knack for conducting targeted searches and filtering through vast amounts of data to find relevant insights.',
+            tools=[tavily],
+            allow_delegation = True
             )
     def master_agent(self):
         return Agent(
             role='Master Agent',
-            goal='Check the output of the other tasks and prioritize which answer is the best suitable for the query asked by the user, adding details and citations as needed.',
+            goal='Check the output of the other tasks and prioritize which answer is the best suitable for the query asked by the user, adding details.Giving an answer along with list of urls and pdf from where the answer was taken from.',
             backstory='master_agent is a proficient legal detailed context summarizer with the ability to synthesize information from various sources to deliver the most accurate and comprehensive response.',
             
         )
