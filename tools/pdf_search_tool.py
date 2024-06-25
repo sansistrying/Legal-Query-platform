35b5b44f4b5b622a281a1667b571293fd646d986
SANSITA KARTHIKEYAN <114069040+sansistrying@users.noreply.github.com>
2024-06-25 20:25:16 +0530

    Update pdf_search_tool.py

--- tools/pdf_search_tool.py
+++ tools/pdf_search_tool.py
@@ -1,60 +1,91 @@
 from crewai_tools import BaseTool
 import os
 from langchain.chains import RetrievalQA
-from PyPDF2 import PdfReader
-from langchain.text_splitter import CharacterTextSplitter, RecursiveCharacterTextSplitter
+from langchain.text_splitter import CharacterTextSplitter, RecursiveCharacterTextSplitter, RecursiveJsonSplitter
 from langchain_community.llms import OpenAI
-from langchain.chains.question_answering import load_qa_chain
-from langchain_community.embeddings.openai import OpenAIEmbeddings
-from langchain_community.vectorstores import ElasticVectorSearch, Pinecone, Weaviate, FAISS
-from langchain_openai import ChatOpenAI
+from langchain_groq import ChatGroq
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain_community.vectorstores import Qdrant
+
 from langchain.prompts import PromptTemplate
 
+# from llama_parse import LlamaParse
+# import joblib
+# from llama_index.core import SimpleDirectoryReader
+from langchain_community.document_loaders import UnstructuredMarkdownLoader, JSONLoader, DirectoryLoader
+from langchain_community.vectorstores import Chroma
+
+from sentence_transformers import SentenceTransformer
+from qdrant_client import QdrantClient
+
+from langchain_ai21 import AI21Embeddings
+
+
+from ai21 import AI21Client
+from ai21.models import EmbedType
+
 class PDF_search(BaseTool):
-    name: str = "PDF reader"  # Name of the tool
-    description: str = "Access the documents and answer the query, also give the corresponding document name."  # Description of the tool
+    name: str = "PDF reader"
+    description: str = "Access the documents and answer the query, also give the corresponding document name only not the path."
 
     def _run(self, user_query: str) -> str:
-        # Load all PDF documents from the specified directory
-        loader = PyPDFDirectoryLoader(r"C:\Users\sansi\OneDrive\Desktop\Collyear_Law_Project\pdf")
-        documents = loader.load()
 
-        # Split the loaded documents into smaller chunks
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
-        all_splits = text_splitter.split_documents(documents)
+        # loader = DirectoryLoader('./json', glob = "**/*.json", show_progress = True, loader_cls = JSONLoader, loader_kwargs = {'jq_schema':'.content', 'text_content': False})
+        # json_data = loader.load()
 
-        # Initialize OpenAI embeddings
-        embeddings = OpenAIEmbeddings()
 
-        # Create a Qdrant vector store from the document splits
-        db = Qdrant.from_documents(all_splits, embeddings, location=":memory:", collection_name="all_splits", distance_func="Dot")
+        # splitter = RecursiveJsonSplitter(max_chunk_size=300)
 
-        # Define the prompt template for the QA task
+        # # Recursively split json data - If you need to access/manipulate the smaller json chunks
+        # json_chunks = splitter.split_json(json_data=json_data)
+
+
+   #     model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2', device='cpu')
+
+       # embeddings = HuggingFaceEmbeddings(model_name="BAAI/bge-small-en-v1.5")
+        #embeddings = SentenceTransformerEmbeddings(model="all-MiniLM-L6-v2")
+        embeddings = AI21Embeddings()
+
+        if "qdrant_db_pdfs" not in os.listdir():
+            loader = PyPDFDirectoryLoader("/home/palak/Desktop/Collyear_Law_Project/pdf")
+            documents = loader.load()
+
+            text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap = 0)
+            all_splits = text_splitter.split_documents(documents)
+
+            os.mkdir("./qdrant_db_pdfs")
+            
+            db = Qdrant.from_documents(all_splits, embeddings, path = "./qdrant_db_pdfs", collection_name = "pdfs", distance_func="Dot")
+
+        else:
+            client = QdrantClient(path = "./qdrant_db_pdfs")
+            db = Qdrant(client = client, collection_name = "pdfs", embeddings=embeddings, )
+
+
+        print('3')
+
         prompt_template = """Text: {context}
 
         Question: {question}
 
         Answer the question based on the PDF Document provided. If the text doesn't contain the answer, reply that the answer is not available.
-        Do Not Hallucinate"""
+        Do Not Hallucinate. Just give one accurate answer."""
 
-        # Create a PromptTemplate object with the defined template
+
         PROMPT = PromptTemplate(
             template=prompt_template, input_variables=["context", "query"]
         )
-        chain_type_kwargs = {"prompt": PROMPT}  # Additional keyword arguments for the chain
+        chain_type_kwargs = {"prompt": PROMPT}
+        
+                               
+        qa = RetrievalQA.from_chain_type(llm = ChatGroq(temperature=0, model_name="llama3-70b-8192"),
+                                        chain_type="stuff",
+                                        input_key = 'query',
+                                        retriever=db.as_retriever(),
+                                        chain_type_kwargs=chain_type_kwargs,
+                                        return_source_documents=True)
 
-        # Initialize the RetrievalQA chain with the specified LLM, retriever, and prompt
-        qa = RetrievalQA.from_chain_type(
-            llm=ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0125", api_key="your-api-key"),
-            chain_type="stuff",
-            input_key='query',
-            retriever=db.as_retriever(),
-            chain_type_kwargs=chain_type_kwargs,
-            return_source_documents=True
-        )
 
-        # Pass the user query to the QA object and get the result
+        # Pass the query to the qa object using the correct key
         result = qa({'query': user_query})
-        return result  # Return the result
+        return result
