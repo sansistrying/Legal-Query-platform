e12cb1960b64f6c238d046d02de52034aff48a71
SANSITA KARTHIKEYAN <114069040+sansistrying@users.noreply.github.com>
2024-06-25 20:24:20 +0530

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -2,19 +2,20 @@ from crewai import Task
 from langchain_community.tools.tavily_search import TavilySearchResults
 from tools.website_search_tool import website_search
 from tools.pdf_search_tool import PDF_search
-
 class Law_Tasks():
     def website_search_task(self,agent,query):
         return Task(
-            description = 'Find answer to query asked. Query is as follows, {query}',
-            expected_output = 'An answer to the question in 2 to 3 lines',
+            description = 'Find answer to query if possible. If answer is I do not know stop and move on to next task. Do not hallucinate. Query is as follows, {query}',
+            expected_output = 'An answer to the question which is taken from the data provided',
             agent = agent
         )
     def web_search_task(self,agent,query):
+       
         return Task(
             description = 'Find answer to query asked. Query is as follows, {query}',
-            expected_output = 'An answer to the question in 2 to 3 lines and a list of the urls from where the content was taken from.',
-            agent = agent   )
+            expected_output = 'An answer to the question in 2 to 3 lines if found and a list of the urls from where the content was taken from. Do not reuse the same input, just give the answer directly',
+            agent = agent,
+               )
     
     def pdf_search_task(self,agent,query):      
         return Task(
@@ -22,4 +23,3 @@ class Law_Tasks():
             expected_output = 'An answer to the question in 2 to 3 lines.',
             agent = agent
         )
-    
